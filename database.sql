d3d1e0493fa38e6afd90b4ab15d48776091f59f6
PietroDSt <pietro.ba08@gmail.com>
2026-05-22 16:55:31 -0300

    Adição do database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS farmacia;
+USE farmacia;
+
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    fabricante VARCHAR(100) NOT NULL,
+    preco DECIMAL(10,2),
+    estoque INT
+);
