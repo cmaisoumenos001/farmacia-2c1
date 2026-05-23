3a29cbe9bf04dfa8a2503606e83b737e9aafa10b
Otavio <otaviotarallosq@gmail.com>
2026-05-22 22:48:33 -0300

    Add footer

---
+++ includes/Footer.php
@@ -0,0 +1,7 @@
+<!-- rodape.php -->
+    <hr>
+    <footer>
+        <p>&copy; 2026 - Desenvolvido pelos alunos Murilo Felipe, Otavio e Pietro da turma de desenvolvimento de sistemas 2°C¹ - Etec Vasco Antônio Venchiarutti</p>
+    </footer>
+</body>
+</html>
