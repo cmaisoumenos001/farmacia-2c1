df7bc453b59acf76e7dd09454d8187639c47cec1
Murilo <mufe.cremonesi@gmail.com>
2026-05-22 22:51:09 -0300

    add header.php

---
+++ includes/Header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Style.css">
+    <title>Farmacia</title>
+  
+</head>
+<body>
+    <header>
+        <h1>Farmacia</h1>
+    <nav>
+        <a href="index.php">Produtos</a> 
+        <a href="cadastro.php">Cadastrar</a>
+    </nav>
+    <hr>
